f50888087c71a8694966645b807559c8f19994ed
3ypup <posilay.pochashe@gmail.com>
2018-11-21 11:20:38 +0300

    model post validation added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  validates :title, presence: {:message => "Выберите тему поста!!"}
+  validates :title, :length => { :maximum => 40, :message => "Слишком длинная тема поста !!"}
+  validates :author, presence: true
+  validates :content, presence: {:message => "Напишите текст поста!!"}
+  validates :title, :length => { :minimum => 123, :message => "Слишком короткий пост!! НУжно больше буков!!"}
+
+  validates :image, presence:  {:message => "Посту нужна обложка!!"}
+  validates_format_of :image,  :with => %r{(png|jpg|jpeg)}i, :message => "Неправильный формат файла !!"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_11_21_081046) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "author"
+    t.text "content"
+    t.text "title"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
