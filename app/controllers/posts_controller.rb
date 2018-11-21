b60f490fe0a758d8c279f6fba454a4790c52fc57
3ypup <posilay.pochashe@gmail.com>
2018-11-21 11:43:50 +0300

    action new post and form added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,15 +6,25 @@ end
 
 
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def new
+    @post = Post.new
+
   end
 
   def create
+    @post = Post.new(post_params)
+    
+    if  @post.save
+        redirect_to @post
+      else
+        render action: 'new'
+      end
   end
 
   def destroy
