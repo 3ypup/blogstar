c69fdbf2974c1d83ef4def20b456a6df12254d2a
3ypup <posilay.pochashe@gmail.com>
2018-11-21 11:23:57 +0300

    controller posts actions added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+
+  def post_params
+    params.require(:post).permit(:author, :content, :title, :image)
+  end
+  
 end
